3606a950aea67a376dee486ea7c6dadc7debbbd8
Paul Salcedo <psalcedodev@gmail.com>
2025-03-29 02:58:03 -0600

    :rocket:

---
+++ app/Http/Controllers/SkillTaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSkillTaskRequest;
+use App\Http\Requests\UpdateSkillTaskRequest;
+use App\Models\SkillTask;
+
+class SkillTaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSkillTaskRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SkillTask $skillTask)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SkillTask $skillTask)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSkillTaskRequest $request, SkillTask $skillTask)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SkillTask $skillTask)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TokenTransactionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTokenTransactionRequest;
+use App\Http\Requests\UpdateTokenTransactionRequest;
+use App\Models\TokenTransaction;
+
+class TokenTransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTokenTransactionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TokenTransaction $tokenTransaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TokenTransaction $tokenTransaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTokenTransactionRequest $request, TokenTransaction $tokenTransaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TokenTransaction $tokenTransaction)
+    {
+        //
+    }
+}
