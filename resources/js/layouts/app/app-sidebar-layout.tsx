465812f5241305fc48525264e98c0b887e5ec8be
Paul Salcedo <psalcedodev@gmail.com>
2025-04-01 17:38:47 -0600

    save point

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -1,15 +1,19 @@
 import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
+import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { useIsMobile } from '@/hooks/use-mobile';
 import { type BreadcrumbItem } from '@/types';
 import { type PropsWithChildren } from 'react';
 
 export default function AppSidebarLayout({ children, breadcrumbs = [] }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
+  const isMobile = useIsMobile();
+
   return (
     <AppShell variant="sidebar">
       <AppSidebar />
       <AppContent variant="sidebar">
-        {/* <AppSidebarHeader breadcrumbs={breadcrumbs} /> */}
+        {isMobile && <AppSidebarHeader breadcrumbs={breadcrumbs} />}
         {children}
       </AppContent>
     </AppShell>
